3b77e5fa568e9e64adacac31ea7fc528b3f766e8
CarlyA <andre375@msu.edu>
2022-01-31 14:43:16 -0500

    1/31 updates

---
+++ Scripts/ggp-app04.r
@@ -0,0 +1,39 @@
+# FW891  January 21 2022
+### Week 4 Homework
+### Carly Andrews 
+
+9 - Application
+Create a script file in your Project's script folder called ggp-app04.r and, using the data from Lansing2016NOAA.csv:
+
+
+Create as many plots as you want and use any data frame to fulfill the requirements:
+
+Create unique plots that, combined, have the following:
+
+6 modifications to plot points using at least 3 unique properties (color, size...)
+
+6 modifications to plot lines using at least 3 unique properties (linetype, color...)
+
+Modifications to 8 unique element_text() subcomponent
+
+Modifications to 4 unique element_line() subcomponent
+
+Modifications to 4 unique element_rect() subcomponent
+
+5 uses of Unicode
+
+4 uses of rgb() colors
+
+1 use of alpha
+
+2 uses of greyscale
+
+Report:
+
+In comments at the end of your script, copy the above and explain how each was used. 
+
+9.1 - Questions
+What was your level of comfort with the lesson/application?
+What areas of the lesson/application confused or still confuses you?
+What is a way you can apply the material in this lesson towards your research or area of study?
+What are some things you would like to learn related to, but not covered in, this lesson?
